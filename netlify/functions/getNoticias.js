a09c412c5b6ae53edf03c87b3e9100a1f4c7e6aa
Jonnes-Santos <jonjonnes123@gmail.com>
2025-03-19 23:25:54 -0300

    Corrigido caminho das funções e adicionada dependência @supabase/supabase-js

--- netlify/functions/getNoticias.js
+++ netlify/functions/getNoticias.js
@@ -1,19 +1,18 @@
-// netlify/functions/getNews.js
+// netlify/functions/getNoticias.js
 const { createClient } = require("@supabase/supabase-js");
 
 // Configurações do Supabase
-const supabaseUrl = process.env.SUPABASE_URL; // URL do seu projeto no Supabase
-const supabaseKey = process.env.SUPABASE_KEY; // Chave pública do Supabase
+const supabaseUrl = process.env.SUPABASE_URL;
+const supabaseKey = process.env.SUPABASE_KEY;
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 exports.handler = async (event, context) => {
   try {
     // Busca as notícias na tabela "noticias" do Supabase
     const { data, error } = await supabase
-      .from("noticias") // Nome da tabela no Supabase
-      .select("*"); // Seleciona todas as colunas
+      .from("noticias")
+      .select("*");
 
-    // Se houver erro na consulta, retorna uma mensagem de erro
     if (error) {
       return {
         statusCode: 500,
@@ -21,13 +20,11 @@ exports.handler = async (event, context) => {
       };
     }
 
-    // Retorna as notícias em formato JSON
     return {
       statusCode: 200,
-      body: JSON.stringify(data), // Dados das notícias
+      body: JSON.stringify(data),
     };
   } catch (error) {
-    // Se ocorrer um erro inesperado, retorna uma mensagem de erro
     return {
       statusCode: 500,
       body: JSON.stringify({ error: "Erro interno no servidor" }),
